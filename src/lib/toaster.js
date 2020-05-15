9672281b7f4001ca770ec140fde5a2732ef996c4
Nick Hoang <nickhoang11@gmail.com>
2020-05-15 12:28:07 -0700

    Remove console log

--- src/lib/toaster.js
+++ src/lib/toaster.js
@@ -1,5 +1,4 @@
 function toaster(text) {
-  console.log('toaster runs ', text)
   document.getElementById(
     'error-toaster',
   ).innerHTML = `<div class="warning-toaster-text">WARNING!!!</div><div class="warning-toaster-content">${text}</div>`
