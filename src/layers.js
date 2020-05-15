66d3a3e6406fbf4cd363c2c763725c7442a5b964
captaincoordinates <tom@sparkgeo.com>
2020-05-15 12:14:58 -0700

    Implemented drag-reordering in layers list

--- src/layers.js
+++ src/layers.js
@@ -27,7 +27,10 @@ function initializeLayers() {
         return `
           <div
             class="visible-layer"
-            id="visible-layer-${visibleLayerName}">
+            id="visible-layer-${visibleLayerName}"
+            draggable="true"
+            ondragover="document.dispatchEvent(new CustomEvent('dragOver', {detail: event}))"
+            ondragstart="document.dispatchEvent(new CustomEvent('dragStart', {detail: event}))">
             <div
               class="visible-layer-name">
               ${visibleLayerName} Layer
@@ -47,6 +50,32 @@ function initializeLayers() {
       return !filterFeatureByGeometryType(feature, data.detail)
     })
   })
+  let draggedElement
+  document.addEventListener('dragStart', (event) => {
+    const dragEvent = event.detail
+    dragEvent.dataTransfer.effectAllowed = 'move'
+    dragEvent.dataTransfer.setData('text/plain', null) // Thanks to bqlou for their comment.
+    draggedElement = dragEvent.target
+  })
+  document.addEventListener('dragOver', (event) => {
+    const dragEvent = event.detail
+    if (dragEvent.target.parentNode !== draggedElement) {
+      if (isBefore(draggedElement, dragEvent.target)) {
+        dragEvent.target.parentNode.parentNode.insertBefore(
+          draggedElement,
+          dragEvent.target.parentNode,
+        )
+      } else {
+        dragEvent.target.parentNode.parentNode.insertBefore(
+          draggedElement,
+          dragEvent.target.parentNode.nextSibling,
+        )
+      }
+    }
+  })
+  document.addEventListener('dragover', function (e) {
+    e.preventDefault()
+  })
   const dndReplaceCheckbox = document.getElementById('dnd-replace')
   dndReplaceCheckbox.checked = defaultSwapStrategy === SwapStrategy.REPLACE
   dndReplaceCheckbox.onchange = function () {
@@ -56,6 +85,21 @@ function initializeLayers() {
 
 function filterFeatureByGeometryType(feature, geometryType) {
   return feature.geometry.type === geometryType
+}
+
+function isBefore(el1, el2) {
+  if (el2.parentNode.parentNode === el1.parentNode) {
+    for (
+      let cur = el1.previousSibling;
+      cur && cur.nodeType !== 9;
+      cur = cur.previousSibling
+    ) {
+      if (cur === el2.parentNode) {
+        return true
+      }
+    }
+  }
+  return false
 }
 
 export default initializeLayers
